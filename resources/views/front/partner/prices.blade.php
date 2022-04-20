b28e3e3da86309b3b7eca3841aa7288696dc35fd
kyslyi <inbox@izzi.studio>
2022-04-20 11:00:26 +0300

    fix

--- resources/views/front/partner/prices.blade.php
+++ resources/views/front/partner/prices.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-3">
     <ul class="partner-prices">
-        <li class="partner-prices__item">Malerarbeiten: <span>{{ Setting::getByKey('system.price.'.auth()->user()->subscription_id.'.cost_malar') }} Chf</span></li>
+        <li class="partner-prices__item">Malerarbeiten: <span>{{ Setting::getByKey('system.price.'.auth()->user()->subscription_id.'.cost_paint_work') }} Chf</span></li>
         <li class="partner-prices__item">Umzug + Reinigung: <span>{{ Setting::getByKey('system.price.'.auth()->user()->subscription_id.'.cost_transfer_cleaning') }} Chf</span></li>
         <li class="partner-prices__item">Umzug: <span>{{ Setting::getByKey('system.price.'.auth()->user()->subscription_id.'.cost_transfer') }} Chf</span></li>
         <li class="partner-prices__item">Reinigung: <span>{{ Setting::getByKey('system.price.'.auth()->user()->subscription_id.'.cost_cleaning') }} Chf</span></li>
