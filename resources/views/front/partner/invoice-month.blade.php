16a0b92deec80735cf3d2cfe1f99ca541b24fcb5
devops@izzi.studio <4WcvcRS6heLbkbM>
2022-05-06 12:52:47 +0300

    fix invoice

--- resources/views/front/partner/invoice-month.blade.php
+++ resources/views/front/partner/invoice-month.blade.php
@@ -125,7 +125,7 @@
                         <td class="title">
                             <img src="./images/logo-red-1.svg" alt="Company logo" style="width: 100%; max-width: 200px" />
                         </td>
- 
+
                         <td>
                             Rechnung #: {{$invoiceNumber}}<br />
                             Erstellt am: {{$fullDate}}<br />
@@ -140,7 +140,7 @@
         <tr class="information">
             <td colspan="3">
                 <table>
-                    <tr> 
+                    <tr>
                         <td>
                             Rahal Gmbh<br />
                             St. Urbanstrasse 79<br />
@@ -196,42 +196,42 @@
                 <td>
                 @if($type_job_id == 1)
 					@php
-						 $costTypeJob = Setting::getByKey('system.setting.cost_transfer');
+						 $costTypeJob = Setting::getByKey('system.price.'.$userSubsId.'.cost_transfer');
 						 $tax = round(($costTypeJob/100) * 7.7, 2);
 						 $nonTaxPrice = round($costTypeJob + $tax, 2);
    				         $taxTotal = $taxTotal + ($tax * ($total / $costTypeJob));
 						 $nonTaxTotal = $nonTaxTotal + (($total / $costTypeJob) * $nonTaxPrice);
 					@endphp
-                    {{$total / $costTypeJob}} * {{$costTypeJob}} CHF				 				
-				
+                    {{$total / $costTypeJob}} * {{$costTypeJob}} CHF
+
                 @endif
 
                 @if($type_job_id == 2)
-                    				 
+
 					@php
-						 $costTypeJob = Setting::getByKey('system.setting.cost_cleaning');
+						 $costTypeJob = Setting::getByKey('system.price.'.$userSubsId.'.cost_cleaning');
 						 $tax = round(($costTypeJob/100) * 7.7, 2);
 						 $nonTaxPrice = round($costTypeJob + $tax, 2);
    				         $taxTotal = $taxTotal + ($tax * ($total / $costTypeJob));
 						 $nonTaxTotal = $nonTaxTotal + (($total / $costTypeJob) * $nonTaxPrice);
 					@endphp
-                    {{$total / $costTypeJob}} * {{$costTypeJob}} CHF				 
+                    {{$total / $costTypeJob}} * {{$costTypeJob}} CHF
                 @endif
 
-                @if($type_job_id == 3)				 
+                @if($type_job_id == 3)
 					@php
-						 $costTypeJob = Setting::getByKey('system.setting.cost_transfer_cleaning');
+						 $costTypeJob = Setting::getByKey('system.price.'.$userSubsId.'.cost_transfer_cleaning');
 						 $tax = round(($costTypeJob/100) * 7.7, 2);
 						 $nonTaxPrice = round($costTypeJob + $tax, 2);
    				         $taxTotal = $taxTotal + ($tax * ($total / $costTypeJob));
 						 $nonTaxTotal = $nonTaxTotal + (($total / $costTypeJob) * $nonTaxPrice);
 					@endphp
-                    {{$total / $costTypeJob}} * {{$costTypeJob}} CHF				 
+                    {{$total / $costTypeJob}} * {{$costTypeJob}} CHF
                 @endif
 
                 @if($type_job_id == 4)
 					@php
-						 $costTypeJob = Setting::getByKey('system.setting.cost_malar');
+						 $costTypeJob = Setting::getByKey('system.price.'.$userSubsId.'.paint_work');
 						 $tax = round(($costTypeJob/100) * 7.7, 2);
 						 $nonTaxPrice = round($costTypeJob + $tax, 2);
    				         $taxTotal = $taxTotal + ($tax * ($total / $costTypeJob));
@@ -254,9 +254,9 @@
 
         <tr class="total">
             <td></td>
-            <td colspan="2" style="text-align: right;">Gesamtbetrag {{ $totals['total'] + $taxTotal }} CHF</td> 
+            <td colspan="2" style="text-align: right;">Gesamtbetrag {{ $totals['total'] + $taxTotal }} CHF</td>
         </tr>
     </table>
 </div>
 </body>
-</html> 
+</html>
